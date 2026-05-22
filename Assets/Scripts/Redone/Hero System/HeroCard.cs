69cedb88d4824565c60818bbe5cc50646112e1e3
billylambe <billy10lambe@hotmail.com>
2026-05-22 16:04:25 +0100

    Health and mana ui for player

--- Assets/Scripts/Redone/Hero System/HeroCard.cs
+++ Assets/Scripts/Redone/Hero System/HeroCard.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using TMPro;
 
 // Represents a player or enemy hero
 public class HeroCard : MonoBehaviour
@@ -8,6 +9,18 @@ public class HeroCard : MonoBehaviour
 
     [Header("Runtime Health")]
     public int currentHealth;
+    public TMP_Text healthText;
+
+    [Header("Runtime Mana")]
+    public int currentMana;
+    public TMP_Text manaText;
+
+    private void FixedUpdate()
+    {
+        healthText.text = currentHealth.ToString();
+        currentMana = GameObject.Find("Mana Manager").GetComponent<ManaManager>().playerMana;
+        manaText.text = currentMana.ToString();
+    }
 
     // Damages this hero
     public void TakeDamage(int amount)
@@ -16,4 +29,11 @@ public class HeroCard : MonoBehaviour
 
         HeroManager.Instance.CheckWinCondition();
     }
+
+    //dynamic health diosplay
+
+    //public void UpdateHealthText(int currentHealth)
+    //{
+    //    healthText.text = ""currentHealth.ToString();
+    //}
 }
