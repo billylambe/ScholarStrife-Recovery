8bf53aad431161e86fd0b9cf876eb761a8b784ee
billylambe <billy10lambe@hotmail.com>
2026-05-22 14:03:24 +0100

    MainMenuAttempt

---
+++ Assets/Scripts/Redone/MenuScripts/GameMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
