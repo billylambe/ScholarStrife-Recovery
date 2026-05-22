1cac311d92534568d0ed66b698d79d704b633199
billylambe <billy10lambe@hotmail.com>
2026-05-22 18:12:50 +0100

    enemy health visuals

---
+++ Assets/Scripts/Redone/Hero System/EnemyHeroCard.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+// Represents a player or enemy hero
+public class EnemyHeroCard : MonoBehaviour
+{
+    [Header("Hero Owner")]
+    public CardOwner owner;
+
+    [Header("Runtime Health")]
+    public int enemycurrentHealth;
+    public TMP_Text enemyhealthText;
+
+    [Header("Runtime Mana")]
+    public int enemycurrentMana;
+    public TMP_Text enemymanaText;
+
+    private void FixedUpdate()
+    {
+        enemyhealthText.text = enemycurrentHealth.ToString();
+        enemycurrentMana = GameObject.Find("Mana Manager").GetComponent<ManaManager>().enemyMana;
+        enemymanaText.text = enemycurrentMana.ToString();
+    }
+
+    // Damages this hero
+    public void TakeDamage(int amount)
+    {
+        enemycurrentHealth -= amount;
+
+        HeroManager.Instance.CheckWinCondition();
+    }
+
+    //dynamic health diosplay
+
+    //public void UpdateHealthText(int currentHealth)
+    //{
+    //    healthText.text = ""currentHealth.ToString();
+    //}
+}
