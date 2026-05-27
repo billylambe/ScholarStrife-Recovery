c20339f3f3383483ea23189ac8b9f0bb65bfc47b
billylambe <billy10lambe@hotmail.com>
2026-05-27 14:04:45 +0100

    TutorialExpanded3

---
+++ Assets/Scripts/Redone/MenuScripts/ResourceRules.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ResourceRules : MonoBehaviour
+{
+
+    [SerializeField] private string gameSceneName;
+    public void resourceRules()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
